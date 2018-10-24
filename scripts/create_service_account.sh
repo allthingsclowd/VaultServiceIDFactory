fed3d5a50e26a41962f5b18753aa5f538bc4ba69
Graham Land <graham@allthingscloud.eu>
2018-10-24 23:13:55 +0100

    Added Consul Connect

--- scripts/create_service_account.sh
+++ scripts/create_service_account.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
-set -x
 
-[ -f /etc/systemd/system/${1}.service ] || sudo tee /etc/systemd/system/${1}.service <<EOF
+create_service () {
+  if [ ! -f /etc/systemd/system/${1}.service ]; then
+    
+    create_service_user ${1}
+    
+    sudo tee /etc/systemd/system/${1}.service <<EOF
 ### BEGIN INIT INFO
 # Provides:          ${1}
 # Required-Start:    $local_fs $remote_fs
 # Required-Stop:     $local_fs $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: ${1} agent
+# Short-Description: ${1} service
 # Description:       ${2}
 ### END INIT INFO
 
 [Unit]
-Description=Consul server agent
+Description=${2}
 Requires=network-online.target
 After=network-online.target
 
@@ -35,11 +39,49 @@ RestartSec=42s
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload
+  sudo systemctl daemon-reload
 
-sudo systemctl start ${1}
+  fi
 
-sudo systemctl status ${1}
+}
+
+create_service_user () {
+  
+  if ! grep ${1} /etc/passwd >/dev/null 2>&1; then
+    echo "Creating ${1} user to run the ${1} service"
+    sudo useradd --system --home /etc/${1}.d --shell /bin/false ${1}
+    sudo mkdir --parents /opt/${1} /usr/local/${1} /etc/${1}.d
+    sudo chown --recursive ${1}:${1} /opt/${1} /etc/${1}.d /usr/local/${1}
+  fi
+
+}
+
+start_app_proxy_service () {
+  # start the new service mesh proxy for the application
+  # param 1 ${1}: app-proxy name
+  # param 2 ${2}: app-proxy service description
+
+  create_service ${1} ${2} "consul connect proxy -sidecar-for ${1}"
+  sudo systemctl start ${1}
+  sudo systemctl status ${1}
+  echo "${1} Proxy App Service Build Complete"
+}
+
+start_client_proxy_service () {
+    # start the new service mesh proxy for the client
+    # param 1 ${1}: client-proxy name
+    # param 2 ${2}: client-proxy service description
+    # param 3 ${3}: client-proxy upstream consul service name
+    # param 4 ${4}: client-proxy local service port number
+    
+
+    create_service ${1} ${2} "consul connect proxy -service ${1} -upstream ${3}:${4}"
+    sudo systemctl start ${1}
+    sudo systemctl status ${1}
+    echo "${1} Proxy Client Service Build Complete"
+}
+
+
 
 
 
